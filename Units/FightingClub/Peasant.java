e9b9d48683c286582d19988074aed322afa3be12
Janna02 <dendy2@yandex.ru>
2023-03-11 10:09:32 +0300

    seminar 3

--- Units/FightingClub/Peasant.java
+++ Units/FightingClub/Peasant.java
@@ -4,18 +4,21 @@ import Units.FightingClubUnits;
 
 public class Peasant extends FightingClubUnits {
 
+    int delivery;
+
     public Peasant(String name) {
         super(name, 1, 1, 1, 1, 3, 1);
+        this.delivery = 1;
     }
 
     @Override
     public String getInfo() {
         return "Крестьянин " + name + " (" + "Атака - " + attack + ", Защита - " + defence + ", Урон - " + minDamage
-                + "-" + maxDamage + ", Здоровье - " + hp + ", Скорость - " + speed;
+                + "-" + maxDamage + ", Здоровье - " + hp + ", Скорость - " + speed + ", Доставка - " + delivery + ")";
     }
 
     @Override
     public String getInfoOfMoves() {
-        return "Крестьянин " + name + " (" + "Скорость - " + speed + ", Здоровье - " + hp + ")";
+        return "Крестьянин " + name + " (" + "Скорость - " + speed  + ", Здоровье - " + hp + ")";
     }
 }
